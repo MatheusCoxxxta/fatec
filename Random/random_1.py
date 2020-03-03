727651829b2bd00ef64a61787074264ac23c6120
Matheus Costa <matheus271200.ms@gmail.com>
2020-03-03 10:08:19 -0300

    Random and Str

---
+++ Random/random_1.py
@@ -0,0 +1,3 @@
+import random
+a = random.sample(range(1, 100), 20)
+print(a)
---
+++ Str/word_1.py
@@ -0,0 +1,8 @@
+word = str(input())
+
+drow = word[::-1]
+
+if(drow != word):
+    print("Não palindrome")
+else:
+    print("Palindrome")
